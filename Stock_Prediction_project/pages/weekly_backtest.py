8cfe7d6447f25d9a04d36a74feac369e9a94b418
Bhavin Mokariya <bhavinkmokariya@gmail.com>
2025-03-05 18:34:57 +0530

    Add files via upload

---
+++ Stock_Prediction_project/pages/weekly_backtest.py
@@ -0,0 +1,3 @@
+# 1) Actual vs Predictions Graph with the difference
+# 2) Comparision of model and accuracy for performance
+# 3) Create a Scheduler after 3:30 for fetching  after market close or real time data
